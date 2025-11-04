ec12169d2e4c50ecc5a486ece5d4e4a41aed041a
Julian <rayconstantyjulian@gmail.com>
2025-11-04 17:15:32 +0100

    Add an axum greet in console

--- uniliste/src/main.js
+++ uniliste/src/main.js
@@ -6,3 +6,14 @@ const app = createApp(App);
 app.use(router);
 app.mount("#app");
 
+fetch(`/api/greet?name=${encodeURIComponent("LoginPage")}`)
+    .then((resp) => {
+        if (!resp.ok) throw new Error(`HTTP ${resp.status} ${resp.statusText}`);
+        return resp.text();
+    })
+    .then((msg) => {
+        console.log(msg);
+    })
+    .catch((e) => {
+        console.error("greet failed", e);
+    });
